9353aaf1e774b1ca783da75d6ff285df08160d51
JoojVitor <joaovitordossantoscouto@gmail.com>
2022-03-16 15:45:02 -0300

    Tratamento CSV

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+doenca_pre = pd.read_csv('casos_obitos_doencas_preexistentes.csv', sep=';', encoding='utf-8')
+doenca_pre.head()
+doenca_pre.shape
 
+from collections import Counter
+Counter(doenca_pre.cs_sexo)
+doenca_pre['cs_sexo'].value_counts()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+doenca_pre.isnull().sum()
 
+# Excluir valor NAN de cs_sexo
+doenca_pre.dropna(subset=['cs_sexo'], inplace=True)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Excluir Ignorado
+relacao = doenca_pre.loc[doenca_pre.cs_sexo != 'IGNORADO']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Excluir Indefinido
+relacao = relacao.loc[relacao.cs_sexo != 'INDEFINIDO']
+relacao['cs_sexo'].value_counts()
+import plotly.express as px
+px.pie(relacao, names="cs_sexo")
+
+# Análise dos óbitos
+relacao.obito.value_counts()
+px.pie(relacao, names="obito")
+
+# Análise da classificação dos atributos
+relacao.dtypes
+
+# Renomeando os registros da variável obito
+relacao["obito"] = relacao["obito"].replace({0:"nao", 1:"sim"})
+relacao.head()
+relacao.dtypes
+relacao.obito.value_counts()
+
+# Transformando em variáveis categóricas
+relacao['cs_sexo'] = relacao['cs_sexo'].astype('category')
+relacao['obito'] = relacao['obito'].astype('category')
